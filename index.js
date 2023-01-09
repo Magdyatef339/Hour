bd4af2bca1b97fd229e672029065c516ef6064ed
Magdy Atef , Frontend Web Developer & MERN STACK Developer <102534938+Magdyatef339@users.noreply.github.com>
2023-01-09 06:31:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector("[data-hour-hand]");
+const minuteHand = document.querySelector("[data-minute-hand]");
+const secondHand = document.querySelector("[data-second-hand]");
+
+function setClock() {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+setClock();
+
+// Dark Mood
+
+let dark = document.getElementById("dark");
+let body = document.getElementById("body");
+
+dark.addEventListener("click", (eo) => {
+  body.classList.toggle("dark")
+});
